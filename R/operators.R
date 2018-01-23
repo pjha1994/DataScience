7d13e99a38410f98f5fc74e51886f1bd830e0b58
abhi-jha <abhi_jha1995@rediffmail.com>
2018-01-24 05:09:10 +0530

    operators

---
+++ R/operators.R
@@ -0,0 +1,53 @@
+x <- 1+2
+y <- 1-2
+z <- 1*2
+a <- 1/2
+b <- 2^2
+c <- 2**2
+d <- 2%%3
+e <- 2%/%3
+1+2
+format(10^5, scientific = FALSE)
+typeof(format(10^5, scientific = FALSE))
+abs(-5)
+log(2)
+log(2,base=10)
+exp(5)
+factorial(5)
+pi
+options()
+options(digits=22,verbose = TRUE)
+pi
+1/0
+-1/0
+1/-0
+-1/-0
+NaN
+Inf+5
+is.finite(1/0)
+is.infinite(1/-0)
+Inf/Inf
+is.nan(Inf/Inf)
+NA+5
+a<-NA
+is.na(a)
+is.nan(NaN)
+is.na(NaN)
+
+1||3
+1&3
+1&&3
+5>2  
+5>=2
+5<2
+5<=2
+5==2
+5!=2
+"b">"a"
+!TRUE
+!(FALSE)
+TRUE | TRUE
+TRUE & FALSE
+
+
+#vectorized operations
